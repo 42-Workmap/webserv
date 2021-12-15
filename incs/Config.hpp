0612997edc9ecc50f31e4afb6d3eca22f540ec2f
zittoooo <stellar7484@naver.com>
2021-12-15 20:35:25 +0900

    add get set Config

--- incs/Config.hpp
+++ incs/Config.hpp
@@ -17,24 +17,31 @@
 class Config 
 {
 	private:
-		Config();
 		Config(const Config &other);
 		
 
 		Webserv *m_webserv;
 		static Config *m_config;
-		std::map<std::string, Server> m_serverMap;
+		std::map<std::string, Server> m_server_map;
 		std::map<std::string, std::string> m_mime_type;
 		std::map<std::string, std::string> m_status_code;
 
 	public:
+		Config();
 		virtual ~Config();
 		Config &operator=(const Config &other);
 
 		static Config* getConfig();
 		void parsingConfig(std::string path);
 
-		//get set 
+		//get
+		Webserv* getWebserv();
+		std::map<std::string, Server> getServerMap();
+		std::map<std::string, std::string> getMimeType();
+		std::map<std::string, std::string> getStatusCode();
+
+		//set
+		void setWebserv(Webserv* webserv);
 };
 
 #endif
--- srcs/Config.cpp
+++ srcs/Config.cpp
@@ -1,5 +1,55 @@
 #include "../incs/Config.hpp"
 
+Config::Config()
+{
+}
+
+Config::~Config()
+{
+	delete this->m_config;
+}
+
+// Config::Config(const Config &other)
+// {
+// 	*this = other;
+// }
+
+// Config& Config::operator=(const Config & other)
+// {
+// 	this->m_webserv = other.m_webserv;
+// 	this->m_server_map = other.m_server_map;
+// 	this->m_mime_type = other.m_mime_type;
+// 	this->m_status_code = other.m_status_code;
+// 	return *this;
+// }
+
+Webserv* Config::getWebserv()
+{
+	return this->m_webserv;
+}
+
+std::map<std::string, Server> Config::getServerMap()
+{
+	return this->m_server_map;
+}
+
+std::map<std::string, std::string> Config::getMimeType()
+{
+	return this->m_mime_type;
+}
+
+std::map<std::string, std::string> Config::getStatusCode()
+{
+	return this->m_status_code;
+}
+
+
+void Config::setWebserv(Webserv* webserv)
+{
+	this->m_webserv = webserv;
+}
+
+
 void 	Config::parsingConfig(std::string path)
 {
 	std::ifstream output;
